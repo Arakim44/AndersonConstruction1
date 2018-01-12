dffff5ed84d02115d1a265393620954c8ebe316b
Ara Kim <ahrakim44@gmail.com>
2018-01-12 02:34:39 -0600

    mobile first banner image responsive works

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,12 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssvars, nested, autoprefixer])) //css autoprefix and simple vars also nested
+        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer])) //css autoprefix and simple vars also nested
         .on('error',function (errorInfo) {
             console.log(errorInfo.toString()); //consoles the error info that is readable.
             this.emit('end'); //even with error, watch task won't stop.
