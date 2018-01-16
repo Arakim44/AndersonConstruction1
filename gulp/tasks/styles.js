bb168cc56c64a98bf7f59f30fafec13f14749d22
Ara Kim <ahrakim44@gmail.com>
2018-01-15 20:55:29 -0600

    mainYellow changed to mainBlue. header not complte

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,12 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer])) //css autoprefix and simple vars also nested
+        .pipe(postcss([cssImport, mixins, cssvars, nested,hexrgba, autoprefixer])) //css autoprefix and simple vars also nested
         .on('error',function (errorInfo) {
             console.log(errorInfo.toString()); //consoles the error info that is readable.
             this.emit('end'); //even with error, watch task won't stop.
