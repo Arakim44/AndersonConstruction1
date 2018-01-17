dbca6e338bec51704ba005eb25addcb257de8d15
Ara Kim <ahrakim44@gmail.com>
2018-01-17 12:34:37 -0600

    reveal-on-scroll compelte.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
-
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 //need to create new object
 var mobileMenu = new MobileMenu();
+
+new RevealOnScroll($(".feature-item"),"85%");
+new RevealOnScroll($(".members"),"60%");
+// var revealOnScroll = new RevealOnScroll();
